4ac71a5f9f737ac0079e6333557da450ba23d957
cassiocoltri <cassiocoltri@gmail.com>
2024-11-12 13:49:40 -0300

    primeiro commit

---
+++ src/main/java/com/devsuperior/dslist_game/DslistGameApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslist_game/DslistGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
