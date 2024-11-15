0fef4dbb5636dd52c8692209edd11fa888aa92d6
cassiocoltri <cassiocoltri@gmail.com>
2024-11-15 09:47:17 -0300

    Game seeding e alterando tipos de descrição para TEXT no lugar de varchar(255)

--- src/main/java/com/devsuperior/dslist_game/entities/Game.java
+++ src/main/java/com/devsuperior/dslist_game/entities/Game.java
@@ -15,21 +15,25 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
     public Game(Integer id, String title, Integer year, String genre,
-                String plataform, String imgUrl, String shortDescription, String longDescription) {
+                String plataforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = plataforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -67,12 +71,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
