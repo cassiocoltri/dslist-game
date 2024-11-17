0f2b650def3243314be2d3a6cb0ea26c919996b8
cassiocoltri <cassiocoltri@gmail.com>
2024-11-17 10:33:56 -0300

    adicionado Projection (interface) e consulta sql no banco

--- src/main/java/com/devsuperior/dslist_game/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist_game/dto/GameMinDTO.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist_game.dto;
 
 
 import com.devsuperior.dslist_game.entities.Game;
+import com.devsuperior.dslist_game.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = Math.toIntExact(projection.getId());
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Integer getId() {
--- src/main/java/com/devsuperior/dslist_game/services/GameService.java
+++ src/main/java/com/devsuperior/dslist_game/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist_game.services;
 import com.devsuperior.dslist_game.dto.GameDTO;
 import com.devsuperior.dslist_game.dto.GameMinDTO;
 import com.devsuperior.dslist_game.entities.Game;
+import com.devsuperior.dslist_game.projections.GameMinProjection;
 import com.devsuperior.dslist_game.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(g -> new GameMinDTO(g)).toList();
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(g -> new GameMinDTO(g)).toList();
 
     }
