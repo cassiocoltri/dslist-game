518098eded112e540e4346c8a1a363be7bd21ab8
cassiocoltri <cassiocoltri@gmail.com>
2024-11-17 09:46:27 -0300

    Get game by id

--- src/main/java/com/devsuperior/dslist_game/services/GameService.java
+++ src/main/java/com/devsuperior/dslist_game/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist_game.services;
 
+import com.devsuperior.dslist_game.dto.GameDTO;
 import com.devsuperior.dslist_game.dto.GameMinDTO;
 import com.devsuperior.dslist_game.entities.Game;
 import com.devsuperior.dslist_game.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    // A anotação Transactional do spring quando colocado readoonly true ele está avisando que é só
+    // "Leitura". não vai escrever nada no banco ou fazer qualquer alteração, vai deixar o cod mais rápido.
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(g -> new GameMinDTO(g)).toList();
